fb24ad97040cdae1505c9f1fd8cdf6ba6e209ac8
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-04-16 12:37:31 +0600

    Tuple Method Examples with a note.

---
+++ 1. Basics/4. Python Data structures/Tuples-Methods.md
@@ -0,0 +1,7 @@
+**Python Tuple** `count()`
+
+    returns count of the element in the tuple
+
+**Python Tuple** `index()`
+
+    returns the index of the element in the tuple
---
+++ 1. Basics/4. Python Data structures/Tuples-Methods.py
@@ -0,0 +1,7 @@
+animals = tuple(('Bear', 'Tiger', 'Lion', 'Elephant', 'Bear', 'Tiger', 'Lion', 'Elephant', 'Bear', 'Tiger', 'Lion', 'Elephant', 'Bear', 'Tiger', 'Lion', 'Elephant', 'Bear', 'Tiger', 'Lion', 'Elephant', 'Bear', 'Tiger', 'Lion', 'Elephant'))
+
+print(animals.index('Tiger'))
+
+print(animals.count('Lion'))
+
+del animals
