f45f53b8f007bef89a89fc716847399db387e75a
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-04-20 00:09:42 +0600

    Inheritance part-04

--- 2. OOP/3. Inheritance/Inheritance.py
+++ 2. OOP/3. Inheritance/Inheritance.py
@@ -4,7 +4,10 @@ class Person:
         self.lname = lname
 
     def printName(self):
-        print(f'\nYour name is {self.fname} {self.lname}')
+        if self.lname is None:
+            print(f'\nYour name is {self.fname}')
+        else:
+            print(f'\nYour name is {self.fname} {self.lname}')
 
 
 R = Person("Sowat", "Rafi")
@@ -12,16 +15,14 @@ R.printName()
 
 
 class Engineers(Person):
-# It is inheriting from the Person (Parent Class)
-    # def __init__(self, fname=None, lname=None):
-        # Person.__init__(self, fname, lname)
+    def __init__(self, fname, projectType, lname=None):
+        Person.__init__(self, fname, lname)  # It is inheriting from the Person (Parent Class)
+        self.projectType = projectType
 
     def printInformation(self):
-        if self.lname is None:
-            print(f'\nYour name is {self.fname}')
-        else:
-            print(f'\nYour name is {self.fname} {self.lname}')
+        self.printName()  # Calling the Parent class method
+        print(f"He is being specialized on {employee_01.projectType}")
 
 
-employee_01 = Engineers("Engr.", "Rafi")
+employee_01 = Engineers(fname="Engr.", lname="Rafi", projectType="Web Applications")
 employee_01.printInformation()
