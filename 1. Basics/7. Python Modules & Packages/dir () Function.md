0008a53d5505506f282e64ae6724853d5569a496
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-04-20 15:01:00 +0600

    All about dir() functions.

---
+++ 1. Basics/7. Python Modules & Packages/dir () Function.md
@@ -0,0 +1,4 @@
+`dir ()` **Function**
+
+* This is a built-in function used to list objects defined inside a module.
+* It can be used on custom and built-in modules.
---
+++ 1. Basics/7. Python Modules & Packages/dir-function.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+m = dir(random)
+mat = dir(math)
+msg = "e"
+other = dir(msg.capitalize())
+
+print(m)
+print(mat)
+
+"""
+Basically printing the usable methods/ magic methods with it.
+"""
