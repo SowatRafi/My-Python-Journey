3cf1eefd8fe286d877466b6582c15d00ca1f7f1c
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-04-24 02:25:00 +0600

    another dataset has been downloaded through this.

--- 1. Basics/10. Working with Images/image_downloader.py
+++ 1. Basics/10. Working with Images/image_downloader.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 from bing_image_downloader import downloader
-downloader.download("1", limit=20, output_dir="Images")
+downloader.download("3", limit=20, output_dir="Images")
