a372b863648ca5f65bde19f486553780784539b9
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-05-03 00:44:09 +0600

    Displaying data from database.

--- Projects/2. Django/1. To Do List/ToDoList_APP/views.py
+++ Projects/2. Django/1. To Do List/ToDoList_APP/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import ToDoList
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, "ToDoList_APP/index.html")
+    todo_items = ToDoList.objects.order_by('id')
+    context = {'todo_items': todo_items}
+    return render(request, "ToDoList_APP/index.html", context)
